49ac5436d54a6dd95e3fc023349b3081aa8d7c52
FelipeAlves99 <falves99.dev@gmail.com>
2020-09-21 21:47:15 -0300

    Alterado estrutura de pastas e criado um novo form para o client

--- src/APS_6.Client/Views/frmMain.cs
+++ src/APS_6.Client/Views/frmMain.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace APS_6.Client
+namespace APS_6.Client.Views
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        public frmMain()
         {
             InitializeComponent();
         }
