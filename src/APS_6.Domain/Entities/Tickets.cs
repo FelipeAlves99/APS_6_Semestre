3da49448cf6e791ad03b81030093d63e357c84e7
FelipeAlves99 <falves99.dev@gmail.com>
2020-09-21 21:48:19 -0300

    Adicionado nova entidade para multas das empresas

---
+++ src/APS_6.Domain/Entities/Tickets.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace APS_6.Domain.Entities
+{
+    public class Tickets
+    {
+        public int Id { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime DeadlineDate { get; set; }
+        public double Value { get; set; }
+        public string Description { get; set; }
+        public int CompanyId { get; set; }
+        public RuralProperty RuralProperty { get; set; }
+    }
+}
