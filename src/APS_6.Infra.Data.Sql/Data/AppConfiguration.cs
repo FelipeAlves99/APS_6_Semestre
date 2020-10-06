fe02a49c0f22e2dcde6f5f4cb46bcf08d0612bc9
FelipeAlves99 <falves99.dev@gmail.com>
2020-10-05 21:05:13 -0300

    Created Entity framework config and first test migrations

---
+++ src/APS_6.Infra.Data.Sql/Data/AppConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace APS_6.Infra.Data.Sql.Data
+{
+    public class AppConfiguration
+    {
+        public AppConfiguration()
+        {
+            //var configurationBuilder = new ConfigurationBuilder();
+            //var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+            //configurationBuilder.AddJsonFile(path, false);
+
+            //var root = configurationBuilder.Build();
+            //ConnectionString = root.GetSection("ConnectionStrings").GetSection("DefaultConnection").Value;            
+        }
+
+        public string ConnectionString { get; } = "Server=localhost\\SQLEXPRESS;Initial Catalog=APS6;Integrated Security=False;User ID=sa;Password=1q2w3e%&!";
+    }
+}
---
+++ src/APS_6.Infra.Data.Sql/Data/Context/APSContext.cs
@@ -0,0 +1,32 @@
+﻿using APS_6.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APS_6.Infra.Data.Sql.Data.Context
+{
+    public class APSContext : DbContext
+    {
+        AppConfiguration appConfiguration = new AppConfiguration();
+
+        public APSContext() : base()
+        { }
+
+        public APSContext(DbContextOptions<APSContext> options) : base(options)
+        { }
+
+        public DbSet<User> Users { get; set; }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+            
+        //}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.UseSqlServer(appConfiguration.ConnectionString);
+        }
+    }
+}
