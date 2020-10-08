54380d13e5528832dcbdf0610aa0c539e8ac9501
FelipeAlves99 <falves99.dev@gmail.com>
2020-10-07 22:15:08 -0300

    Created user constructor and user repository interface to test EF Core

--- src/APS_6.Domain/Entities/User.cs
+++ src/APS_6.Domain/Entities/User.cs
@@ -4,10 +4,19 @@ namespace APS_6.Domain.Entities
 {
     public class User
     {
+        public User(string userName, string password, string name, EAccessLevel permission)
+        {
+
+            UserName = userName;
+            Password = password;
+            Name = name;
+            Permission = permission;
+        }
+
         public int Id { get; private set; }
         public string UserName { get; private set; }
         public string Password { get; private set; }
         public string Name { get; private set; }
-        public EAccessLevel Permission { get; private set; }         
+        public EAccessLevel Permission { get; private set; }
     }
 }
---
+++ src/APS_6.Domain/Interfaces/Repository/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using APS_6.Domain.Entities;
+
+namespace APS_6.Domain.Interfaces.Repository
+{
+    public interface IUserRepository
+    {
+        void AddUser(User user);
+    }
+}
