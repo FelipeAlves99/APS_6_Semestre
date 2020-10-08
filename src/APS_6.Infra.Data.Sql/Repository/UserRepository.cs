8dbd45480af53d0ad621382b1d64100fb394efba
FelipeAlves99 <falves99.dev@gmail.com>
2020-10-07 22:19:38 -0300

    Added the add method for testing EF Core

---
+++ src/APS_6.Infra.Data.Sql/Repository/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using APS_6.Domain.Entities;
+using APS_6.Domain.Interfaces.Repository;
+using APS_6.Infra.Data.Sql.Data.Context;
+using System;
+
+namespace APS_6.Infra.Data.Sql.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly APSContext _context = new APSContext();
+
+        //public UserRepository(APSContext context)
+        //{
+        //    //_context = context;
+        //}
+
+        public void AddUser(User user)
+        {
+            _context.Set<User>().Add(user);
+            _context.SaveChanges();
+        }
+    }
+}
