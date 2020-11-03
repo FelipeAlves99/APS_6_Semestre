c8518376c377c92c9ba2660f57b5331ec7120558
Felipe Alves <falves99.dev@gmail.com>
2020-11-02 21:36:07 -0300

    added pesticides interfaces

---
+++ src/APS_6.Domain/Interfaces/Repository/IPesticideRepository.cs
@@ -0,0 +1,14 @@
+﻿using APS_6.Domain.Entities;
+using System.Collections.Generic;
+
+namespace APS_6.Domain.Interfaces.Repository
+{
+    public interface IPesticideRepository
+    {
+        void PostPesticide(Pesticide pesticide);
+        void PutPesticide(Pesticide pesticide);
+        IEnumerable<Pesticide> GetAllPesticides();
+        Pesticide GetPesticideByName(string name);
+        Pesticide GetPesticidesByCompanyName(string name);
+    }
+}
---
+++ src/APS_6.Domain/Interfaces/Services/IPesticideService.cs
@@ -0,0 +1,14 @@
+﻿using APS_6.Domain.Entities;
+using System.Collections.Generic;
+
+namespace APS_6.Domain.Interfaces.Services
+{
+    public interface IPesticideService
+    {
+        void PostPesticide(Pesticide pesticide);
+        void PutPesticide(Pesticide pesticide);
+        IEnumerable<Pesticide> GetAllPesticides();
+        Pesticide GetPesticideByName(string name);
+        Pesticide GetPesticidesByCompanyName(string name);
+    }
+}
