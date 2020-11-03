aaad934e7a5f7ef412edbc2b5acfca7f86be33b3
Felipe Alves <falves99.dev@gmail.com>
2020-11-02 21:38:01 -0300

    Applied pesticides interfaces

---
+++ src/APS_6.Domain/Services/PesticideService.cs
@@ -0,0 +1,32 @@
+﻿using APS_6.Domain.Entities;
+using APS_6.Domain.Interfaces.Repository;
+using APS_6.Domain.Interfaces.Services;
+using System.Collections.Generic;
+
+namespace APS_6.Domain.Services
+{
+    public class PesticideService : IPesticideService
+    {
+        private readonly IPesticideRepository _pesticideRepository;
+
+        public PesticideService(IPesticideRepository pesticideRepository)
+        {
+            _pesticideRepository = pesticideRepository;
+        }
+
+        public IEnumerable<Pesticide> GetAllPesticides()
+            => _pesticideRepository.GetAllPesticides();
+
+        public Pesticide GetPesticideByName(string name)
+            => _pesticideRepository.GetPesticideByName(name);
+
+        public Pesticide GetPesticidesByCompanyName(string name)
+            => _pesticideRepository.GetPesticidesByCompanyName(name);
+
+        public void PostPesticide(Pesticide pesticide)
+            => _pesticideRepository.PostPesticide(pesticide);
+
+        public void PutPesticide(Pesticide pesticide)
+            => _pesticideRepository.PutPesticide(pesticide);
+    }
+}
---
+++ src/APS_6.Infra.Data.Sql/Repository/PesticideRepository.cs
@@ -0,0 +1,50 @@
+﻿using APS_6.Domain.Entities;
+using APS_6.Domain.Interfaces.Repository;
+using APS_6.Infra.Data.Sql.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace APS_6.Infra.Data.Sql.Repository
+{
+    public class PesticideRepository : IPesticideRepository
+    {
+        private readonly APSContext _context;
+
+        public PesticideRepository(APSContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Pesticide> GetAllPesticides()
+            => _context.Pesticides
+                .AsNoTracking()
+                .Include(p => p.RuralProperty)
+                .ToList();
+
+        public Pesticide GetPesticideByName(string name)
+            => _context.Pesticides
+                .AsNoTracking()
+                .Include(p => p.RuralProperty)
+                .FirstOrDefault(p => p.Name == name);
+
+        public Pesticide GetPesticidesByCompanyName(string name)
+            => _context.Pesticides
+                .AsNoTracking()
+                .Include(p => p.RuralProperty)
+                .FirstOrDefault(p => p.RuralProperty.CompanyName == name);
+
+        public void PostPesticide(Pesticide pesticide)
+        {
+            _context.Set<Pesticide>().Add(pesticide);
+            _context.SaveChanges();
+        }
+
+        public void PutPesticide(Pesticide pesticide)
+        {
+            _context.Attach(pesticide);
+            _context.Entry(pesticide).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+    }
+}
