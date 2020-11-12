6ac11a8d7189cba737ec15204b7ffdd8264de747
FelipeAlves99 <falves99.dev@gmail.com>
2020-11-11 23:55:48 -0300

    Added rural property interfaces

---
+++ src/APS_6.Domain/Interfaces/Repository/IRuralPropertyRepository.cs
@@ -0,0 +1,13 @@
+﻿using APS_6.Domain.Entities;
+using System.Collections.Generic;
+
+namespace APS_6.Domain.Interfaces.Repository
+{
+    public interface IRuralPropertyRepository
+    {
+        void PostRuralProperty(RuralProperty ruralProperty);
+        void PutRuralProperty(RuralProperty ruralProperty);
+        IEnumerable<RuralProperty> GetAllRuralProperty();
+        RuralProperty GetPRuralPropertyByName(string name);
+    }
+}
---
+++ src/APS_6.Domain/Interfaces/Services/IRuralPropertyService.cs
@@ -0,0 +1,13 @@
+﻿using APS_6.Domain.Entities;
+using System.Collections.Generic;
+
+namespace APS_6.Domain.Interfaces.Services
+{
+    public interface IRuralPropertyService
+    {
+        void PostRuralProperty(RuralProperty ruralProperty);
+        void PutRuralProperty(RuralProperty ruralProperty);
+        IEnumerable<RuralProperty> GetAllRuralProperty();
+        RuralProperty GetPRuralPropertyByName(string name);
+    }
+}
