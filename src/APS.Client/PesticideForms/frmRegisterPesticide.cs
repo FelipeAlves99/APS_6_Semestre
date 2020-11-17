03be891756981ddf49e3711cca6b60eb694fdce3
FelipeAlves99 <falves99.dev@gmail.com>
2020-11-17 00:09:09 -0300

    Creation of the forms projects

---
+++ src/APS.Client/PesticideForms/frmRegisterPesticide.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace APS.Client.PesticideForms
+{
+    public partial class frmRegisterPesticide : Form
+    {
+        public frmRegisterPesticide()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/APS.Client/PropertyForms/frmRegisterProperty.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace APS.Client.PropertyForms
+{
+    public partial class frmRegisterProperty : Form
+    {
+        public frmRegisterProperty()
+        {
+            InitializeComponent();
+        }
+    }
+}
